ea182b686dcfba201d1c223f221db02510bdc90e
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-08-09 21:27:11 +0300

    Add files via upload

---
+++ First_web-main/README.md
@@ -0,0 +1,5 @@
+DevStack community is a community of young developer mainly students at the univesity.
+This project is a portfolio Web that will be showcasing the profile for Devstack members and thier entire work.
+The community is working to collaborate on quite alot of projects.
+
+This project is a collaboration of its members, who are committed and interest is on computing technology
---
+++ First_web-main/about.js
@@ -0,0 +1,20 @@
+/*===============Toggle Icon Navbar==========*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+    myFunction();
+};
+
+
+
+function myFunction() {
+    var x = document.getElementById("menu");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
